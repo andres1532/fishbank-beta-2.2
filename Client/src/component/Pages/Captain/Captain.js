35500efc392bfda7298f41b65bb0923942c95e48
juanhc1498 <juanhc1498@gmail.com>
2021-05-31 15:28:38 -0500

    pruebas de barcos en react

--- Client/src/component/Pages/Captain/Captain.js
+++ Client/src/component/Pages/Captain/Captain.js
@@ -2,11 +2,12 @@ import { useState, useEffect, useRef } from "react";
 import Sketch from "react-p5";
 //import p5Types from "p5";
 import p5 from "p5";
+import imgb from "../../../assets/imagenes/ship.svg";
 
 export default function Captain() {
 
-    class Ship {
-    constructor(
+    const Ship = ( 
+      p,
       x,
       y,
       img,
@@ -15,36 +16,43 @@ export default function Captain() {
       progressbarGas,
       progressbarFish,
       select
-    ) {
-      this.x = x;
-      this.y = y;
-      this.img = img;
-      this.level = level;
-      this.direction = direction;
-      this.progressbarGas = progressbarGas;
-      this.progressbarFish = progressbarFish;
-      this.select = select;
+    ) => {
+
+    const body = () => {
+      let ShipImg = p.loadImage(img, img => {
+        p.redraw(); // <- only if you're running with noLoop()
+      });
+
+      
+      p.fill(200,50,200);
+      p.rect(x, y, 50, 50);
+      p.background(ShipImg);
+      
+      p.image(ShipImg);
+      //console.log(p.image);
+      //debugger;
+      ShipImg.resize(50, 30);
+      //console.log(ShipImg);
+      //image(ShipImg, this.x1, this.y1);
+
     }
 
-    body() {
-      //ShipImg = loadImage('Assets/images/Sprite1.png');
-      p5.rect(this.x, this.y, 50, 50);
-    }
-
-    keyPressed(keyCode) {
-      console.log(keyCode);
+    const keyPressed = (keyCode) => {
+     // console.log(keyCode);
       if (keyCode === "LEFT_ARROW") {
         ships1.push({ ships1 });
         console.log(ships1);
       }
 
       if (keyCode === "UP_ARROW") {
-        ships2.push(new ships2());
+        ships2.push(ships2());
         console.log(ships2);
       }
     }
+        console.log("fin del proyecto")
+    return{body,keyPressed};
   }
-  //------------------------------Ship
+  //------------------------------fin Ship
   
 
 
@@ -52,52 +60,60 @@ export default function Captain() {
 
   const [ships1, setShips1] = useState([]);
   const [ships2, setShips2] = useState([]);
-
-  useEffect(() => {
-    console.log("ff");
-    new p5((p) => {
+  let state={ships1xx: [], ships2xx: []}
+  const Sketch = (p) => {
 
 
-        p.setGradient = (x, y, w, h, ColorIzquierdo, ColorDerecho) => {
-            
-            for (let i = x; i <= x + w; i++){
-                let inter = p.map(i, x, x + w, 0, 1);
-                let c = p.lerpColor(ColorIzquierdo, ColorDerecho, inter);
-                p.stroke(c);
-                p.line(i, y, i, y + h);
-            }
-          
-        };
+    /*p.setGradient = (x, y, w, h, ColorIzquierdo, ColorDerecho) => {
+        
+        for (let i = x; i <= x + w; i++){
+            let inter = p.map(i, x, x + w, 0, 1);
+            let c = p.lerpColor(ColorIzquierdo, ColorDerecho, inter);
+            p.stroke(c);
+            p.line(i, y, i, y + h);
+        }
+      
+    };*/
 
-      p.setup = () => {
-        //p.background("#ff0000");
+     p.setup = () => {
+         
+        let temp1 = [], temp2 = [];
 
         p.createCanvas(window.innerWidth, window.innerHeight);
         for (let i = 0; i < 1; i++) {
-          setShips1(new Ship(50, 50, 0, 1));
-          setShips2(new Ship(50, 150, 0, 2));
+        temp1.push(Ship(p,50, 50, imgb, 1));
+        temp2.push(Ship(p,50, 150, imgb, 2));
         }
 
-        //definicion de colores por RGB
-        //const ColorDerecho = p.color(0,0,50);
-        //const ColorIzquierdo = p.color(0, 100, 255);
+        setShips1(temp1);
+        setShips2(temp2);
+        console.log(temp1);
+        state.ships1xx = temp1;
+        state.ships2xx = temp2;
 
-      };
+        console.log(state.ships2xx[0].body);
+    };
 
-      p.draw = () => {
-        //p.background(100,250,100);
-        p.setGradient(0, 0, window.innerWidth, 675, p.color(0, 100, 255), p.color(0,0,50), 0, 2);//(posicion en x, posicion en y, ancho, alto)
-        if (ships1.length > 0 && ships1.length > 0) {
-          for (let i = 0; i < 1; i++) {
-            ships1[i].body();
-            ships2[i].body();
+  p.draw = () => {
+    p.background(220);
+    //console.log(ships1);
+        //p.setGradient(0, 0, window.innerWidth, window.innerHeight, p.color(0, 100, 255), p.color(0,0,50), 0, 2);//(posicion en x, posicion en y, ancho, alto)
+        if (state.ships1xx.length > 0 && state.ships2xx.length > 0) {
+            //console.log("hh");
+            for (let i = 0; i < 1; i++) {
+                state.ships1xx[i].body();
+                state.ships2xx[i].body();
 
-            //ships1[i].keyPressed();
-          }
+                state.ships1xx[i].keyPressed();
+            }
         }
-      };
+    };
 
-    }, processingRef.current);
+}
+  
+  useEffect(() => {
+    console.log("ff");
+    new p5(Sketch, processingRef.current);
   }, []);
 
   return (
